721de87c79f25efd60b326a0b9ad4ddbef1b972a
Farooq-Muhammad <167761676+Farooq-Muhammad@users.noreply.github.com>
2024-05-01 14:12:43 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,28 @@
+// Question 1
+//console.log("helloe world");
+//QUESTION 2
+//let personName: string = ("Aric")
+//console.log("hellow",personName,"Would you like to learn some python langauge");
+//QUESTION NO 3"
+//let PersonName:string = "farooq"
+//console.log("lowercase:",PersonName. toLocaleLowerCase());
+//console.log("uppercase:",PersonName.toUpperCase());
+//console.log("titlecase:",personName.replace(/\bw/g,c => c.toLocaleUpperCase()));
+
+// Question no 4
+//let quote:string ="A person who made a mistake never tried anything new";
+//let famous_person:string = "Albert Einstein";
+//let message:string= `${famous_person} once said, ${quote}`;
+//console.log(message);
+//question no 5
+//let Famous_person:string="albert einstein"
+//let Message:string= `${famous_person} once said, ${quote}`;
+
+
+
+//queston no 6
+let personName ="\n\tfarooq muhmmad\t\n";
+console.log(personName);
+var stripped = personName.trim();
+console.log(stripped);
+
